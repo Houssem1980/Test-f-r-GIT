ce6b83e93a74ea600b7ae78ee84bb196d1a01ad4
Houssem1980 <70571201+Houssem1980@users.noreply.github.com>
2020-09-01 11:17:03 +0200

    Add project files.

---
+++ Test för GIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_för_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
